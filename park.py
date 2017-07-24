96a3059666e5585fea7891dac00517113bd770b1
Theriley106 <christopherlambert106@gmail.com>
2017-07-23 21:29:19 -0400

    Reorganized park.py

--- park.py
+++ park.py
@@ -3,43 +3,47 @@ import requests
 import os
 import yagmail
 import time
-import RandomHeaders
 
-myemail = str(open(“send.txt”, “r”).read()).partition(':')[0]
-password = str(open(“send.txt”, “r”).read()).partition(':')[2]
-sendemail = list(str(open(“to.txt”, “r”).read()))
-
-yagmail.register(myemail, password)
-yag = yagmail.SMTP(myemail)
+WAIT = 15
 
 
-if os.path.isfile('status.txt') == False:
-    os.system("touch status.txt")
-def Changed(newtext, myemail=myemail, sendemail=sendemail):
+def Email(newtext, myemail=None, password=None, sendemail=None):
+    if myemail == None:
+        myemail = str(open(“send.txt”, “r”).read()).partition(':')[0]
+    if password == None:
+        password = str(open(“send.txt”, “r”).read()).partition(':')[2]
+    if sendemail == None:
+        sendemail = list(str(open(“to.txt”, “r”).read()))
+    yagmail.register(myemail, password)
+    yag = yagmail.SMTP(myemail)
     text_file = open("status.txt", "w")
     text_file.write(newtext)
     text_file.close()
     yagmail.SMTP(myemail).send(sendemail, 'Change Detected {} Spots Now Available for 7/29'.format(newtext), 'Click here: https://www.recreation.gov/permitCalendar.do?page=calendar&calarvdate=07/16/2017&contractCode=NRSO&parkId=72201\nTo Purchase the tickets')
-    yagmail.SMTP(myemail).send(sendemailtwo, 'Change Detected {} Spots Now Available for 7/29'.format(newtext), 'Click here: https://www.recreation.gov/permitCalendar.do?page=calendar&calarvdate=07/16/2017&contractCode=NRSO&parkId=72201\nTo Purchase the tickets')
     print('sent email')
 
+def resetFile():
+    if os.path.isfile('status.txt') == True:
+        os.remove('status.txt')
+    os.system("touch status.txt")
 
-while True:
+
+def returnActionToken(page):
+    for tag in page.find_all("input", type="hidden"):
+        if 'actionToken' in str(tag):
+            return str(str(tag).partition("value='")[2]).partition("' name='")[0]
+
+def CheckChange():
     res = requests.session()
-    e = bs4.BeautifulSoup(res.get('https://www.recreation.gov/permitCalendar.do?page=calendar&calarvdate=07/16/2017&contractCode=NRSO&parkId=72201').text)
 
     data = {'permitTypeId':'1034702728',
     'targetUIID':'entrance',
     'contractCode':'NRSO',
     'facilityId':'72201',
     'titleText':'Any Trail/Zone'}
+
     a = res.post('https://www.recreation.gov/ajax/EntranceList', data=data)
-    a = bs4.BeautifulSoup(a.text, 'lxml')
-    hidden_tags = e.find_all("input", type="hidden")
-    for tag in hidden_tags:
-        print(tag)
-        if 'actionToken' in str(tag):
-            actionToken = str(str(tag).partition("value='")[2]).partition("' name='")[0]
+
     data = {'contractCode':'NRSO',
     'performSearch':'true',
     'pageOrigin':'permitCalendar',
@@ -56,13 +60,20 @@ while True:
     'watercraftSize':'',
     'cartItemId': '',
     'availStatus': '',
-    'actionToken': actionToken}
-    a = res.post('https://www.recreation.gov/permitCalendar.do?mode=submit', data=data)
-    a = res.get('https://www.recreation.gov/permits/Mt_Whitney/r/permitCalendar.do?page=calendar&contractCode=NRSO&parkId=72201')
-    page = bs4.BeautifulSoup(a.text, 'lxml')
+    'actionToken': returnActionToken(bs4.BeautifulSoup(res.get('https://www.recreation.gov/permitCalendar.do?page=calendar&calarvdate=07/16/2017&contractCode=NRSO&parkId=72201').text))}
+    
+    res.post('https://www.recreation.gov/permitCalendar.do?mode=submit', data=data)
+    page = res.get('https://www.recreation.gov/permits/Mt_Whitney/r/permitCalendar.do?page=calendar&contractCode=NRSO&parkId=72201')
+    page = bs4.BeautifulSoup(page.text, 'lxml')
     with open('status.txt') as f:
         if page.select('.permitStatus')[1].getText() not in f.readlines():
-            Changed(str(page.select('.permitStatus')[1].getText()))
-    time.sleep(60 * 15)
+            return str(page.select('.permitStatus')[1].getText())
+    return None
 
-
+if __name__ == "__main__":
+    resetFile()
+    while True:
+        A = CheckChange()
+        if A != None:
+            Email(A)
+        time.sleep(60 * WAIT)
